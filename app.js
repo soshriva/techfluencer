7b55de627edcd816125ecbf3b4d50042bd9d032f
soshriva <yoursaurav2006@gmail.com>
2026-08-06 00:41:07 +0800

    Load Blog 4 mobile polish and source cleanup

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (() => {
   const loaderScript = document.currentScript;
   const baseUrl = new URL(".", loaderScript?.src || location.href);
-  const version = "20260806-0028";
+  const version = "20260806-0037";
 
   const loadScript = (file, onload) => {
     const script = document.createElement("script");
@@ -19,7 +19,9 @@
       loadScript("blog4-knowledge.js", () =>
         loadScript("blog4-final-review.js", () =>
           loadScript("blog4-toc.js", () =>
-            loadScript("blog4-design-clarity.js", () => loadScript("blog4-challenge.js"))
+            loadScript("blog4-design-clarity.js", () =>
+              loadScript("blog4-challenge.js", () => loadScript("blog4-mobile-polish.js"))
+            )
           )
         )
       )
